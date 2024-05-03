839dfb1524d4ac690017168cd7e40fa20307270b
mjeong@ualberta.ca <mjeong@ualberta.ca>
2024-05-03 15:47:47 -0600

    dd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
     <BrowserRouter>
     <div className="App">
     <HomepageNav/>
-    <Profile/>
     <Routes>
     <Route exact path ="/" element =  {<Profile/>}> </Route>
     <Route path ="/projects" element =  {<ProjectList/>}> </Route>
